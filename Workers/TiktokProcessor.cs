ee183e3fe8e745f5190cb739d1963bbbad8ae905
Oleksandr Savchenko <oleksandr.savchenko12@gmail.com>
2025-04-27 17:50:07 +0300

    First commit

---
+++ Workers/TiktokProcessor.cs
@@ -0,0 +1,9 @@
+﻿namespace TiktokWorker.Workers;
+
+public class TiktokProcessor : BackgroundService
+{
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        return Task.CompletedTask;
+    }
+}
